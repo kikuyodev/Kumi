2695396f9033d6e4e2c4a93d8fef0262de45e8f4
Ryōshū <kyoko.laica@gmail.com>
2023-10-27 19:52:39 -0400

    Add test scenes

---
+++ Kumi.Tests/Visual/KumiTestScene.cs
@@ -0,0 +1,22 @@
+﻿using osu.Framework.Testing;
+
+namespace Kumi.Tests.Visual;
+
+public partial class KumiTestScene : TestScene
+{
+    protected override ITestSceneTestRunner CreateRunner() => new KumiTestSceneTestRunner();
+
+    private partial class KumiTestSceneTestRunner : KumiGameBase, ITestSceneTestRunner
+    {
+        private TestSceneTestRunner.TestRunner runner = null!;
+
+        protected override void LoadAsyncComplete()
+        {
+            base.LoadAsyncComplete();
+            Add(runner = new TestSceneTestRunner.TestRunner());
+        }
+
+        public void RunTestBlocking(TestScene test)
+            => runner.RunTestBlocking(test);
+    }
+}
---
+++ Kumi.Tests/Visual/TestSceneKumiGame.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Platform;
+
+namespace Kumi.Tests.Visual;
+
+[TestFixture]
+public partial class TestSceneKumiGame : KumiTestScene
+{
+    private KumiGame game = null!;
+
+    [BackgroundDependencyLoader]
+    private void load(GameHost host)
+    {
+        game = new KumiGame();
+        game.SetHost(host);
+        
+        AddGame(game);
+    }
+}
