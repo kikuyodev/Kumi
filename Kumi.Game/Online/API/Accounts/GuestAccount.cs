d6a77515c388c7f820d36d69e9c01af7eb9f205b
Irisu <nekomarisakirisame@gmail.com>
2023-10-30 15:48:28 -0400

    Add basic API account & guest account

---
+++ Kumi.Game/Online/API/Accounts/GuestAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace Kumi.Game.Online.API.Users;
+
+public class GuestAccount : APIAccount
+{
+    public GuestAccount()
+    {
+        Id = DEFAULT_SYSTEM_ID;
+        Username = "Guest";
+    }
+}
