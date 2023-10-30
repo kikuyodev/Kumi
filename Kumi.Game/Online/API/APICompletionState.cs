7bfd4efee35de6007d72e29764f5b0b2892e8427
Irisu <nekomarisakirisame@gmail.com>
2023-10-30 15:49:50 -0400

    Add API requests & API connection providers

---
+++ Kumi.Game/Online/API/APICompletionState.cs
@@ -0,0 +1,22 @@
+﻿namespace Kumi.Game.Online.API;
+
+/// <summary>
+/// The state of an API request, ranging from not started to completed.
+/// </summary>
+public enum APICompletionState
+{
+    /// <summary>
+    /// The request is currently being performed, or is waiting to be performed.
+    /// </summary>
+    Performing,
+    
+    /// <summary>
+    /// The request has been completed.
+    /// </summary>
+    Completed,
+    
+    /// <summary>
+    /// The request has failed.
+    /// </summary>
+    Failed
+}
---
+++ Kumi.Game/Online/API/IHasStatus.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Kumi.Game.Online.API;
+
+/// <summary>
+/// An interface for API responses that have a status code and a possible error message.
+/// </summary>
+public interface IHasStatus
+{
+    /// <summary>
+    /// The status code of the response.
+    /// </summary>
+    [JsonProperty("code")]
+    public int StatusCode { get; set; }
+    
+    /// <summary>
+    /// A possible error message, just in case the status code is not 200.
+    /// </summary>
+    [JsonProperty("error")]
+    public string? ErrorMessage { get; set; }
+}
