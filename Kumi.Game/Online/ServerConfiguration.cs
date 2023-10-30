6d20c45c132411dfaed443cdbfe319e016fe5d27
Irisu <nekomarisakirisame@gmail.com>
2023-10-30 15:47:55 -0400

    Add base server configuration class

---
+++ Kumi.Game/Online/ServerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace Kumi.Game.Online;
+
+public class ServerConfiguration
+{
+    /// <summary>
+    /// The base URL for the central REST API.
+    /// </summary>
+    public string APIUri { get; set; }
+    
+    /// <summary>
+    /// The base URL for the central SignalR server.
+    /// </summary
+    public string WebsocketUri { get; set; }
+    
+    /// <summary>
+    /// The base URL for the website.
+    /// </summary>
+    public string WebsiteUri { get; set; }
+}
