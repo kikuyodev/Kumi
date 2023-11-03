c4992f7dc9ef1040b29af7faee35fc9c3065c611
Irisu <nekomarisakirisame@gmail.com>
2023-11-03 13:52:09 -0400

    Remove default accessors from `ITimingPoint`

--- Kumi.Game/Charts/Timings/ITimingPoint.cs
+++ Kumi.Game/Charts/Timings/ITimingPoint.cs
@@ -7,24 +7,24 @@ public interface ITimingPoint : IHasTime
     /// <summary>
     /// The type of timing point this is.
     /// </summary>
-    public TimingPointType PointType { get; }
+    TimingPointType PointType { get; }
 
     /// <summary>
     /// The timing flags of this timing point.
     /// </summary>
-    public TimingFlags Flags { get; }
+    TimingFlags Flags { get; }
 
     /// <summary>
     /// The volume of this timing point.
     /// </summary>
-    public int Volume { get; }
+    int Volume { get; }
     
     /// <summary>
     /// Gets the relative scroll speed of this timing point.
     ///
     /// This can be used to calculate the scroll speed of a <see cref="IHasTime"/> object.
     /// </summary>
-    public float RelativeScrollSpeed { get; }
+    float RelativeScrollSpeed { get; }
 }
 
 public enum TimingPointType
