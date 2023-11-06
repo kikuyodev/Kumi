87ee93889f024bf8a5b21dbbf0529bc343d13d2d
Ryōshū <kyoko.laica@gmail.com>
2023-11-05 19:15:14 -0500

    Add function to load component in game
    This will also allow the logo animation to change it's state depending on how many components are loaded, useful in the future when we want to load components like the music flyout menu, the toolbar, etc.

--- Kumi.Game/KumiGame.cs
+++ Kumi.Game/KumiGame.cs
@@ -1,4 +1,5 @@
-﻿using Kumi.Game.Screens;
+﻿using Kumi.Game.Overlays;
+using Kumi.Game.Screens;
 using Kumi.Game.Screens.Menu;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
@@ -10,6 +11,8 @@ public partial class KumiGame : KumiGameBase
     private KumiScreenStack screenStack = null!;
     private LoaderScreen loaderScreen = null!;
 
+    protected MusicController MusicController { get; private set; } = null!;
+
     [BackgroundDependencyLoader]
     private void load()
     {
@@ -17,51 +20,75 @@ public partial class KumiGame : KumiGameBase
         {
             screenStack = new KumiScreenStack { RelativeSizeAxes = Axes.Both }
         });
-        
+
         DependencyContainer.CacheAs(screenStack.BackgroundStack);
     }
 
     protected override void LoadComplete()
     {
         base.LoadComplete();
-        
+
         LoadComponent(loaderScreen = new LoaderScreen());
         screenStack.Push(loaderScreen);
-        
-        // simulate loading for now
-        using (BeginDelayedSequence(1000))
-        {
-            loaderScreen.SetProgress(0.1f);
-            
-            using (BeginDelayedSequence(1000))
-            {
-                loaderScreen.SetProgress(0.3f);
-                
-                using (BeginDelayedSequence(1000))
-                {
-                    loaderScreen.SetProgress(0.5f);
-                    
-                    using (BeginDelayedSequence(1000))
-                    {
-                        loaderScreen.SetProgress(0.6f);
-                    
-                        using (BeginDelayedSequence(500))
-                        {
-                            loaderScreen.SetProgress(0.9f);
-                    
-                            using (BeginDelayedSequence(1000))
-                            {
-                                loaderScreen.SetProgress(1f);
-                            }
-                        }
-                    }
-                }
-            }
-        }
 
-        Scheduler.AddDelayed(() =>
+        DependencyContainer.CacheAs(this);
+
+        loadComponents(new[]
+        {
+            new ComponentLoadTask<MusicController>(loadComplete: Add)
+        }, () =>
         {
             LoadComponentAsync(new MenuScreen(), screenStack.Push);
-        }, 6500);
+        });
+    }
+
+    private void loadComponents<T>(IReadOnlyList<ComponentLoadTask<T>> components, Action loadComplete)
+        where T : Drawable, new()
+    {
+        var amount = components.Count;
+
+        for (var i = 0; i < amount; i++)
+        {
+            var component = components[i];
+            LoadComponentAsync(component.Drawable, c =>
+            {
+                component.CacheDrawable(DependencyContainer);
+                component.LoadComplete?.Invoke(c);
+            }).Wait();
+            loaderScreen.SetProgress((float) i / amount);
+        }
+
+        loadComplete();
+    }
+
+    private struct ComponentLoadTask<T>
+        where T : Drawable, new()
+    {
+        public T Drawable { get; }
+        public Action<Drawable>? LoadComplete { get; set; }
+
+        private readonly bool cache;
+
+        public ComponentLoadTask(bool cache = true, Action<Drawable>? loadComplete = null)
+        {
+            this.cache = cache;
+            LoadComplete = loadComplete;
+            Drawable = new T();
+        }
+
+        public ComponentLoadTask(T drawable, bool cache = true, Action<Drawable>? loadComplete = null)
+        {
+            this.cache = cache;
+            LoadComplete = loadComplete;
+            Drawable = drawable;
+        }
+
+        public void CacheDrawable(DependencyContainer container)
+        {
+            if (!cache)
+                return;
+
+            container.CacheAs<T>(Drawable);
+        }
     }
 }
