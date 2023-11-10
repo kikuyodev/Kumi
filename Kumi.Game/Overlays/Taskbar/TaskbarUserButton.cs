f54581c8bdc89518885fc26dd4038fba693c406a
Ryōshū <kyoko.laica@gmail.com>
2023-11-10 16:22:05 -0500

    Implement simple taskbar

---
+++ Kumi.Game/Overlays/Taskbar/TaskbarUserButton.cs
@@ -0,0 +1,80 @@
+﻿using Kumi.Game.Graphics;
+using Kumi.Game.Graphics.Sprites;
+using Kumi.Game.Online.API;
+using Kumi.Game.Online.API.Accounts;
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osuTK;
+
+namespace Kumi.Game.Overlays.Taskbar;
+
+public partial class TaskbarUserButton : TaskbarButton
+{
+    private SpriteText username = null!;
+    private UpdateableAvatarSprite avatar = null!;
+
+    [BackgroundDependencyLoader(true)]
+    private void load(IAPIConnectionProvider? connection)
+    {
+        connection?.LocalAccount.BindValueChanged(a =>
+        {
+            username.Text = a.NewValue?.Username ?? "Guest";
+            avatar.Account = a.NewValue ?? new GuestAccount();
+        }, true);
+    }
+
+    protected override Drawable CreateContent()
+        => new FillFlowContainer
+        {
+            Direction = FillDirection.Horizontal,
+            RelativeSizeAxes = Axes.Y,
+            AutoSizeAxes = Axes.X,
+            Spacing = new Vector2(8f, 0),
+            Children = new Drawable[]
+            {
+                new Container
+                {
+                    RelativeSizeAxes = Axes.Y,
+                    AutoSizeAxes = Axes.X,
+                    Child = username = new SpriteText
+                    {
+                        Text = "Guest",
+                        Font = KumiFonts.GetFont(size: 14),
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre
+                    },
+                },
+                new Container
+                {
+                    RelativeSizeAxes = Axes.Y,
+                    AutoSizeAxes = Axes.X,
+                    Children = new Drawable[]
+                    {
+                        new Container
+                        {
+                            Size = new Vector2(32),
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            Masking = true,
+                            CornerRadius = 5,
+                            Child = new Box
+                            {
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Color4Extensions.FromHex("1A1A1A"),
+                            }
+                        },
+                        avatar = new UpdateableAvatarSprite
+                        {
+                            Size = new Vector2(32),
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre
+                        },
+                    }
+                },
+            }
+        };
+}
---
+++ Kumi.Game/Overlays/TaskbarOverlay.cs
@@ -0,0 +1,95 @@
+﻿using Kumi.Game.Overlays.Taskbar;
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+
+namespace Kumi.Game.Overlays;
+
+public partial class TaskbarOverlay : OverlayContainer
+{
+    private FillFlowContainer leftFlow = null!;
+    private FillFlowContainer rightFlow = null!;
+    
+    protected override bool StartHidden => true;
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        RelativeSizeAxes = Axes.X;
+        Height = 48;
+        
+        Children = new Drawable[]
+        {
+            new Box
+            {
+                RelativeSizeAxes = Axes.Both,
+                Colour = Color4Extensions.FromHex("0D0D0D")
+            },
+            new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding
+                {
+                    Horizontal = 40
+                },
+                Children = new Drawable[]
+                {
+                    leftFlow = new FillFlowContainer
+                    {
+                        RelativeSizeAxes = Axes.Y,
+                        AutoSizeAxes = Axes.X
+                    },
+                    rightFlow = new FillFlowContainer
+                    {
+                        RelativeSizeAxes = Axes.Y,
+                        AutoSizeAxes = Axes.X,
+                        Anchor = Anchor.TopRight,
+                        Origin = Anchor.TopRight
+                    }
+                }
+            }
+        };
+        
+        addButton(new TaskbarIconButton
+        {
+            Alignment = TaskbarButtonAlignment.Left,
+            Icon = FontAwesome.Solid.Home
+        });
+        
+        addButton(new TaskbarUserButton
+        {
+            Alignment = TaskbarButtonAlignment.Right,
+        });
+    }
+
+    protected override void PopIn()
+    {
+        this.MoveToY(0, 200, Easing.OutQuint);
+    }
+
+    protected override void PopOut()
+    {
+        this.MoveToY(-DrawHeight, 200, Easing.OutQuint);
+    }
+    
+    private void addButton(TaskbarButton button, Action? action = null)
+    {
+        button.Action = action ?? button.Action;
+        
+        switch (button.Alignment)
+        {
+            case TaskbarButtonAlignment.Left:
+                leftFlow.Add(button);
+                break;
+            case TaskbarButtonAlignment.Right:
+                rightFlow.Add(button);
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
