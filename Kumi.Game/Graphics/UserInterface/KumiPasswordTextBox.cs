3be5f18468b2f1b2a9dfbffb2d3c2892afe45c2c
Ryōshū <kyoko.laica@gmail.com>
2023-11-10 18:04:11 -0500

    Add common user interface elements

---
+++ Kumi.Game/Graphics/UserInterface/KumiPasswordTextBox.cs
@@ -0,0 +1,18 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+
+namespace Kumi.Game.Graphics.UserInterface;
+
+public partial class KumiPasswordTextBox : KumiTextBox
+{
+    protected override Drawable GetDrawableCharacter(char c) => new BasicTextBox.FallingDownContainer
+    {
+        AutoSizeAxes = Axes.Both,
+        Child = new SpriteText
+        {
+            Text = "•",
+            Font = KumiFonts.GetFont(size: CalculatedTextSize)
+        }
+    };
+}
