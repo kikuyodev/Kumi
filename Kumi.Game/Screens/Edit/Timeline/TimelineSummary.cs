83f2ceaf0347b9de0bfc850f2fea9d23c35003a1
Ryōshū <kyoko.laica@gmail.com>
2023-11-30 20:01:59 -0500

    Timeline summary

---
+++ Kumi.Game/Screens/Edit/Timeline/TimelineSummary.cs
@@ -0,0 +1,16 @@
+﻿using Kumi.Game.Screens.Edit.Timeline.Parts;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace Kumi.Game.Screens.Edit.Timeline;
+
+public partial class TimelineSummary : Container
+{
+    public TimelineSummary()
+    {
+        Children = new Drawable[]
+        {
+            new MarkerPart { RelativeSizeAxes = Axes.Both }
+        };
+    }
+}
