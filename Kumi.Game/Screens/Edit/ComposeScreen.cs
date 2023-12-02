3ecaeb064789a146b89a95304558fe92aca76bfe
Ryōshū <kyoko.laica@gmail.com>
2023-12-01 19:02:34 -0500

    Add compose screen

---
+++ Kumi.Game/Screens/Edit/ComposeScreen.cs
@@ -0,0 +1,56 @@
+﻿using Kumi.Game.Charts;
+using Kumi.Game.Gameplay;
+using Kumi.Game.Graphics.Containers;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Threading;
+
+namespace Kumi.Game.Screens.Edit;
+
+public partial class ComposeScreen : EditorScreenWithTimeline
+{
+    private Container playfieldContainer = null!;
+    
+    [Resolved]
+    private IBindable<WorkingChart> workingChart { get; set; } = null!;
+    
+    [Resolved]
+    private EditorClock clock { get; set; } = null!;
+
+    public ComposeScreen()
+        : base(EditorScreenMode.Compose, false)
+    {
+    }
+
+    private ScheduledDelegate? playfieldLoad;
+    
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        playfieldLoad = Scheduler.AddDelayed(() =>
+        {
+            if (!workingChart.Value.ChartLoaded)
+                return;
+
+            playfieldContainer.Add(new InputBlockingContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Depth = 1,
+                Child = new KumiPlayfield(workingChart.Value)
+                {
+                    Clock = clock,
+                    ProcessCustomClock = false
+                }
+            });
+
+            playfieldLoad?.Cancel();
+            playfieldLoad = null;
+        }, 1, true);
+    }
+
+    protected override Drawable CreateMainContent()
+        => playfieldContainer = new Container { RelativeSizeAxes = Axes.Both };
+}
---
+++ Kumi.Game/Screens/Edit/EditorScreenWithTimeline.cs
@@ -0,0 +1,81 @@
+﻿using Kumi.Game.Screens.Edit.Timeline;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace Kumi.Game.Screens.Edit;
+
+public abstract partial class EditorScreenWithTimeline : EditorScreen
+{
+    public const float PADDING = 8;
+
+    public Container TimelineContent { get; private set; } = null!;
+
+    public Container MainContent { get; private set; } = null!;
+
+    protected EditorScreenWithTimeline(EditorScreenMode type, bool pushContent = true)
+        : base(type)
+    {
+        if (pushContent)
+        {
+            InternalChild = new GridContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                RowDimensions = new []
+                {
+                    new Dimension(GridSizeMode.AutoSize),
+                    new Dimension()
+                },
+                Content = new[]
+                {
+                    new[] { createTimelineContent() },
+                    new[] { createMainContent() }
+                }
+            };    
+        }
+        else
+        {
+            InternalChild = new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                Children = new[]
+                {
+                    createTimelineContent(),
+                    CreateMainContent()
+                }
+            };
+        }
+    }
+
+    protected abstract Drawable CreateMainContent();
+    
+    private Drawable createMainContent()
+    {
+        MainContent = new Container
+        {
+            RelativeSizeAxes = Axes.Both,
+            Depth = float.MaxValue
+        };
+
+        Scheduler.AddOnce(() => LoadComponentAsync(CreateMainContent(), content =>
+        {
+            MainContent.Add(content);
+            content.FadeInFromZero(300);
+        }));
+
+        return MainContent;
+    }
+
+    private Drawable createTimelineContent()
+    {
+        TimelineContent = new Container
+        {
+            RelativeSizeAxes = Axes.X,
+            AutoSizeAxes = Axes.Y,
+            Padding = new MarginPadding { Top = 12 + EditorOverlay.TOP_BAR_HEIGHT + PADDING, Horizontal = 12 },
+        };
+
+        Scheduler.AddOnce(() => LoadComponentAsync(new TimelineArea(), TimelineContent.Add));
+
+        return TimelineContent;
+    }
+}
