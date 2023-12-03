5188fa76a7c8981e74c27c4aa32a508995f18cd1
Ryōshū <kyoko.laica@gmail.com>
2023-12-03 17:52:18 -0500

    Beat divisor

---
+++ Kumi.Game/Charts/BeatDivisorCollection.cs
@@ -0,0 +1,32 @@
+﻿namespace Kumi.Game.Charts;
+
+public class BeatDivisorCollection
+{
+    public BeatDivisorType Type { get; }
+    public IReadOnlyList<int> Divisors { get; }
+
+    public BeatDivisorCollection(BeatDivisorType type, IEnumerable<int> divisors)
+    {
+        Type = type;
+        Divisors = divisors.ToArray();
+    }
+    
+    public static readonly BeatDivisorCollection COMMON = new BeatDivisorCollection(BeatDivisorType.Common, new[] { 1, 2, 4, 8, 16 });
+    public static readonly BeatDivisorCollection TRIPLETS = new BeatDivisorCollection(BeatDivisorType.Triplets, new[] { 1, 3, 6, 12 });
+
+    public static BeatDivisorCollection Custom(int maxDivisor)
+    {
+        var divisors = new List<int>();
+
+        for (var i = 1; i < Math.Sqrt(maxDivisor); ++i)
+        {
+            if (maxDivisor % i != 0)
+                continue;
+            
+            divisors.Add(i);
+            divisors.Add(maxDivisor / i);
+        }
+        
+        return new BeatDivisorCollection(BeatDivisorType.Custom, divisors);
+    }
+}
---
+++ Kumi.Game/Charts/BindableBeatDivisor.cs
@@ -0,0 +1,73 @@
+﻿using osu.Framework.Bindables;
+
+namespace Kumi.Game.Charts;
+
+public class BindableBeatDivisor : BindableInt
+{
+    public static readonly int[] PREDEFINED_DIVISORS = { 1, 2, 3, 4, 6, 8, 12, 16 };
+
+    public Bindable<BeatDivisorCollection> ValidDivisors { get; } = new Bindable<BeatDivisorCollection>(BeatDivisorCollection.COMMON);
+    
+    public BindableBeatDivisor(int value = 4)
+        : base(value)
+    {
+        ValidDivisors.BindValueChanged(_ => updateBindableProperties(), true);
+        BindValueChanged(_ => ensureValidDivisor());
+    }
+
+    public void SetArbitraryDivisor(int divisor)
+    {
+        if (!ValidDivisors.Value.Divisors.Contains(divisor))
+        {
+            if (BeatDivisorCollection.COMMON.Divisors.Contains(divisor))
+                ValidDivisors.Value = BeatDivisorCollection.COMMON;
+            else if (BeatDivisorCollection.TRIPLETS.Divisors.Contains(divisor))
+                ValidDivisors.Value = BeatDivisorCollection.TRIPLETS;
+            else
+                ValidDivisors.Value = BeatDivisorCollection.Custom(divisor);
+        }
+        
+        Value = divisor;
+    }
+
+    private void updateBindableProperties()
+    {
+        ensureValidDivisor();
+
+        MinValue = ValidDivisors.Value.Divisors.Min();
+        MaxValue = ValidDivisors.Value.Divisors.Max();
+    }
+
+    private void ensureValidDivisor()
+    {
+        if (!ValidDivisors.Value.Divisors.Contains(Value))
+            Value = 1;
+    }
+
+    protected override int DefaultPrecision => 1;
+
+    public override void BindTo(Bindable<int> them)
+    {
+        if (them is BindableBeatDivisor other)
+            ValidDivisors.BindTo(other.ValidDivisors);
+        
+        base.BindTo(them);
+    }
+
+    protected override Bindable<int> CreateInstance() => new BindableBeatDivisor();
+
+    public static int GetDivisorForBeatIndex(int index, int beatDivisor, int[]? validDivisors = null)
+    {
+        validDivisors ??= PREDEFINED_DIVISORS;
+
+        var beat = index % beatDivisor;
+
+        foreach (var divisor in validDivisors)
+        {
+            if ((beat * divisor) % beatDivisor == 0)
+                return divisor;
+        }
+        
+        return 0;
+    }
+}
