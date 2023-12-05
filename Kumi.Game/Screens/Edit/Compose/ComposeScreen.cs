5189544e82cf1872df2cd49f3bdfa3d103d6b0e2
Ryōshū <kyoko.laica@gmail.com>
2023-12-05 16:07:49 -0500

    Integrate blueprint container into compose screen

--- Kumi.Game/Screens/Edit/Compose/ComposeScreen.cs
+++ Kumi.Game/Screens/Edit/Compose/ComposeScreen.cs
@@ -1,6 +1,9 @@
 ﻿using Kumi.Game.Charts;
 using Kumi.Game.Gameplay;
+using Kumi.Game.Gameplay.Drawables;
 using Kumi.Game.Graphics.Containers;
+using Kumi.Game.Screens.Edit.Blueprints;
+using Kumi.Game.Screens.Edit.Compose;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
@@ -9,15 +12,21 @@ using osu.Framework.Threading;
 
 namespace Kumi.Game.Screens.Edit;
 
+[Cached]
 public partial class ComposeScreen : EditorScreenWithTimeline
 {
     private Container playfieldContainer = null!;
+    private BlueprintContainer blueprintContainer = null!;
+
+    public KumiPlayfield Playfield { get; private set; } = null!;
     
     [Resolved]
     private IBindable<WorkingChart> workingChart { get; set; } = null!;
     
     [Resolved]
     private EditorClock clock { get; set; } = null!;
+
+    public IReadOnlyList<DrawableNote> Notes => Playfield.Notes;
 
     public ComposeScreen()
         : base(EditorScreenMode.Compose, false)
@@ -39,10 +48,19 @@ public partial class ComposeScreen : EditorScreenWithTimeline
             {
                 RelativeSizeAxes = Axes.Both,
                 Depth = 1,
-                Child = new KumiPlayfield(workingChart.Value)
+                Children = new Drawable[]
                 {
-                    Clock = clock,
-                    ProcessCustomClock = false
+                    Playfield = new KumiPlayfield(workingChart.Value)
+                    {
+                        Clock = clock,
+                        ProcessCustomClock = false
+                    },
+                    blueprintContainer = new ComposeBlueprintContainer
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Clock = clock,
+                        ProcessCustomClock = false
+                    }
                 }
             });
 
