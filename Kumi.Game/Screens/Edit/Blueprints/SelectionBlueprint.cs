9b6dec25021cd2ee21b4c4c146e38d833159343b
Ryōshū <kyoko.laica@gmail.com>
2023-12-05 16:04:59 -0500

    Selection blueprint

---
+++ Kumi.Game/Screens/Edit/Blueprints/SelectionBlueprint.cs
@@ -0,0 +1,102 @@
+﻿using osu.Framework;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Primitives;
+using osuTK;
+
+namespace Kumi.Game.Screens.Edit.Blueprints;
+
+public abstract partial class SelectionBlueprint<T> : CompositeDrawable, IStateful<SelectionState>
+{
+    public readonly T Item;
+
+    public event Action<SelectionBlueprint<T>>? Selected;
+    public event Action<SelectionBlueprint<T>>? Deselected;
+
+    public override bool HandlePositionalInput => IsSelectable;
+    public override bool RemoveWhenNotAlive => false;
+
+    protected SelectionBlueprint(T item)
+    {
+        Item = item;
+
+        RelativeSizeAxes = Axes.Both;
+        AlwaysPresent = true;
+    }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+        updateState();
+    }
+
+    private SelectionState state;
+    
+    public event Action<SelectionState>? StateChanged;
+
+    public SelectionState State
+    {
+        get => state;
+        set
+        {
+            if (state == value)
+                return;
+
+            state = value;
+            
+            if (IsLoaded)
+                updateState();
+            
+            StateChanged?.Invoke(state);
+        }
+    }
+
+    private void updateState()
+    {
+        switch (state)
+        {
+            case SelectionState.Selected:
+                OnSelected();
+                Selected?.Invoke(this);
+                break;
+            case SelectionState.NotSelected:
+                OnDeselected();
+                Deselected?.Invoke(this);
+                break;
+        }
+    }
+
+    protected virtual void OnDeselected()
+    {
+        foreach (var d in InternalChildren)
+            d.Hide();
+    }
+
+    protected virtual void OnSelected()
+    {
+        foreach (var d in InternalChildren)
+            d.Show();
+    }
+
+    protected override bool ShouldBeConsideredForInput(Drawable child) => State == SelectionState.Selected;
+
+    public void Select() => State = SelectionState.Selected;
+    
+    public void Deselect() => State = SelectionState.NotSelected;
+
+    public void ToggleSelection() => State = IsSelected ? SelectionState.NotSelected : SelectionState.Selected;
+    
+    public bool IsSelected => State == SelectionState.Selected;
+    
+    public virtual bool IsSelectable => ShouldBeAlive && IsPresent;
+
+    public virtual Vector2 ScreenSpaceSelectionPoint => ScreenSpaceDrawQuad.Centre;
+
+    public virtual Quad SelectionQuad => ScreenSpaceDrawQuad;
+}
+
+public enum SelectionState
+{
+    NotSelected,
+    Selected,
+}
---
+++ Kumi.Game/Screens/Edit/Compose/NoteSelectionBlueprint.cs
@@ -0,0 +1,36 @@
+﻿using Kumi.Game.Charts.Objects;
+using Kumi.Game.Gameplay.Drawables;
+using Kumi.Game.Screens.Edit.Blueprints;
+using osu.Framework.Graphics.Primitives;
+using osuTK;
+
+namespace Kumi.Game.Screens.Edit.Compose;
+
+public abstract partial class NoteSelectionBlueprint : SelectionBlueprint<Note>
+{
+    public DrawableNote DrawableNote { get; internal set; } = null!;
+
+    protected override bool ShouldBeAlive => (DrawableNote?.IsAlive == true && DrawableNote.IsPresent) || (State == SelectionState.Selected);
+
+    protected NoteSelectionBlueprint(Note item)
+        : base(item)
+    {
+    }
+
+    public override bool ReceivePositionalInputAt(Vector2 screenSpacePos)
+        => DrawableNote.ReceivePositionalInputAt(screenSpacePos);
+
+    public override Quad SelectionQuad
+        => DrawableNote.ScreenSpaceDrawQuad;
+}
+
+public abstract partial class NoteSelectionBlueprint<T> : NoteSelectionBlueprint
+    where T : Note
+{
+    protected new T Item => (T)base.Item;
+
+    protected NoteSelectionBlueprint(T item)
+        : base(item)
+    {
+    }
+}
