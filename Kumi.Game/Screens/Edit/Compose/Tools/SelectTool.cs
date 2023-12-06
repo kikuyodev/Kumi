bcf106a91d015ba977304a2e60c207de629de86b
Ryōshū <kyoko.laica@gmail.com>
2023-12-06 16:50:30 -0500

    Placement blueprint

---
+++ Kumi.Game/Screens/Edit/Compose/Tools/SelectTool.cs
@@ -0,0 +1,19 @@
+﻿using Kumi.Game.Screens.Edit.Blueprints;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+
+namespace Kumi.Game.Screens.Edit.Compose.Tools;
+
+public class SelectTool : NoteCompositionTool
+{
+    public SelectTool()
+        : base("Select")
+    {
+    }
+
+    public override Drawable? CreateIcon()
+        => new SpriteIcon { Icon = FontAwesome.Solid.MousePointer };
+
+    public override PlacementBlueprint? CreatePlacementBlueprint()
+        => null;
+}
