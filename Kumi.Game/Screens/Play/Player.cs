086b9ace65424f723b0c6f633267a2f3abd33190
Ryōshū <kyoko.laica@gmail.com>
2023-12-07 16:24:13 -0500

    Player / Player loader

---
+++ Kumi.Game/Screens/Play/Player.cs
@@ -0,0 +1,62 @@
+﻿using Kumi.Game.Charts;
+using Kumi.Game.Gameplay;
+using Kumi.Game.Gameplay.Clocks;
+using Kumi.Game.Input;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Screens;
+
+namespace Kumi.Game.Screens.Play;
+
+public partial class Player : ScreenWithChartBackground
+{
+    public override float BlurAmount => 0.0025f;
+    public override float DimAmount => 0.25f;
+    
+    public bool LoadedChartSuccessfully => playfield?.Notes.Any() == true;
+
+    private KumiPlayfield? playfield;
+    private GameplayClockContainer? clockContainer;
+    private GameplayKeybindContainer? keybindContainer;
+
+    [BackgroundDependencyLoader]
+    private void load(IBindable<WorkingChart> chart)
+    {
+        if (chart.Value is DummyWorkingChart)
+            return;
+
+        InternalChild = createGameplayClockContainer(chart.Value);
+    }
+
+    public override void OnEntering(ScreenTransitionEvent e)
+    {
+        base.OnEntering(e);
+
+        Scheduler.AddDelayed(() =>
+        {
+            if (clockContainer != null)
+                clockContainer.Start();
+        }, 500);
+    }
+
+    private Drawable createGameplayClockContainer(WorkingChart chart, double startDelay = 1000)
+    {
+        if (!chart.TrackLoaded)
+            chart.LoadChartTrack();
+
+        playfield = new KumiPlayfield(chart);
+        keybindContainer = new GameplayKeybindContainer();
+
+        clockContainer = new GameplayClockContainer(chart.Track);
+        clockContainer.Reset(-startDelay);
+
+        keybindContainer.Child = clockContainer;
+        clockContainer.Child = playfield;
+
+        playfield.Clock = clockContainer;
+        playfield.ProcessCustomClock = false;
+
+        return keybindContainer;
+    }
+}
---
+++ Kumi.Game/Screens/Play/PlayerLoader.cs
@@ -0,0 +1,166 @@
+﻿using Kumi.Game.Charts;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Screens;
+using osu.Framework.Threading;
+using osuTK.Graphics;
+
+namespace Kumi.Game.Screens.Play;
+
+public partial class PlayerLoader : ScreenWithChartBackground
+{
+    public override float BlurAmount => 0f;
+    public override float DimAmount => 0.9f;
+
+    private bool readyForPush
+        => player?.LoadState == LoadState.Ready;
+
+    private Task? loadTask;
+
+    private Player? player;
+
+    private ScheduledDelegate? scheduledPushPlayer;
+
+    private Container content = null!;
+    private ChartLoaderDisplay display = null!;
+    private Box background = null!;
+
+    [Resolved]
+    private IBindable<WorkingChart> chart { get; set; } = null!;
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        InternalChildren = new Drawable[]
+        {
+            content = new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                Children = new Drawable[]
+                {
+                    background = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = ColourInfo.GradientVertical(Color4.Black.Opacity(0.75f), Color4.Black.Opacity(0f)),
+                    },
+                    display = new ChartLoaderDisplay(chart.Value.ChartInfo)
+                    {
+                        Anchor = Anchor.TopCentre,
+                        Origin = Anchor.TopCentre
+                    }
+                }
+            }
+        };
+    }
+
+    public override void OnEntering(ScreenTransitionEvent e)
+    {
+        base.OnEntering(e);
+
+        contentIn();
+
+        display.Delay(700).FadeIn(500, Easing.OutQuint);
+
+        Scheduler.Add(new ScheduledDelegate(pushWhenLoaded, Clock.CurrentTime + 1000, 0));
+    }
+
+    public override void OnResuming(ScreenTransitionEvent e)
+    {
+        base.OnResuming(e);
+
+        player = null;
+        cancelLoad();
+
+        contentIn();
+    }
+
+    public override void OnSuspending(ScreenTransitionEvent e)
+    {
+        base.OnSuspending(e);
+
+        chart.Value.Track.Stop();
+    }
+
+    public override bool OnExiting(ScreenExitEvent e)
+    {
+        cancelLoad();
+        contentOut();
+
+        this.Delay(500).FadeOut();
+
+        return base.OnExiting(e);
+    }
+
+    private void pushWhenLoaded()
+    {
+        if (!this.IsCurrentScreen())
+            return;
+
+        if (!readyForPush)
+        {
+            cancelLoad();
+            return;
+        }
+
+        if (scheduledPushPlayer != null)
+            return;
+
+        scheduledPushPlayer = Scheduler.AddDelayed(() =>
+        {
+            if (!this.IsCurrentScreen())
+                return;
+
+            loadTask = null;
+            ValidForResume = false;
+
+            if (player?.LoadedChartSuccessfully ?? false)
+                this.Push(player);
+            else
+                this.Exit();
+        }, 500);
+    }
+
+    private void prepareScreen()
+    {
+        if (!this.IsCurrentScreen())
+            return;
+
+        player = new Player();
+
+        loadTask = LoadComponentAsync(player, _ =>
+        {
+            display.IsReady.Value = true;
+            background.FadeTo(0.75f, 250, Easing.OutQuint);
+            CurrentBackground.BackgroundStack.DimAmount = 0.4f;
+        });
+    }
+
+    private void contentIn()
+    {
+        content.FadeInFromZero(500, Easing.OutQuint).Then().Schedule(prepareScreen);
+    }
+
+    private void contentOut()
+    {
+        content.FadeOut(500, Easing.OutQuint);
+    }
+
+    private void cancelLoad()
+    {
+        scheduledPushPlayer?.Cancel();
+        scheduledPushPlayer = null;
+    }
+
+    protected override void Dispose(bool isDisposing)
+    {
+        base.Dispose(isDisposing);
+
+        if (isDisposing)
+            loadTask?.ContinueWith(_ => player?.Dispose());
+    }
+}
