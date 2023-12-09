4b64c200e4c7ef5119fadbe4f5ee2966c8ff52f6
Irisu <nekomarisakirisame@gmail.com>
2023-12-08 19:02:42 -0500

    Handle each note judgement

--- Kumi.Game/Gameplay/Scoring/ScoreProcessor.cs
+++ Kumi.Game/Gameplay/Scoring/ScoreProcessor.cs
@@ -1,23 +1,51 @@
-﻿using Kumi.Game.Gameplay.Judgements;
+﻿using Kumi.Game.Charts.Objects.Windows;
+using Kumi.Game.Extensions;
+using Kumi.Game.Gameplay.Judgements;
 using Kumi.Game.Scoring;
 
 namespace Kumi.Game.Gameplay.Scoring;
 
 public partial class ScoreProcessor : JudgementProcessor
 {
+    private const long good_score = 300;
+    private const long ok_score = 200;
+    private const long bad_score = 100;
+    
     private long totalScore;
+    private int combo;
+    private int highestCombo;
     
     protected override void ApplyJudgementInternal(Judgement judgement)
     {
         if (IsSimulating)
             return;
-        
-        totalScore += judgement.GetScore();
+
+        if (judgement.Result == NoteHitResult.Miss)
+        {
+            if (combo > highestCombo)
+                highestCombo = combo;
+            
+            combo = 0;
+        } else 
+            combo++;
+
+        if (judgement.Result.AffectsScore())
+        {
+            totalScore += getScoreValueFor(judgement.Result) + Int64.Max(0, (long)Math.Floor(Math.Min(combo, 100) / 10.0));
+        }
     }
 
     public void PopulateScore(ScoreInfo score)
     {
-        score.MaxCombo = JudgedHits;
+        score.MaxCombo = highestCombo;
         score.TotalScore = totalScore;
     }
+    
+    private long getScoreValueFor(NoteHitResult result) => result switch
+    {
+        NoteHitResult.Good => good_score,
+        NoteHitResult.Ok => ok_score,
+        NoteHitResult.Bad => bad_score,
+        _ => 0
+    };
 }
