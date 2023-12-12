4d49481c89cb73ac04a059aed3ecc951e082781c
Irisu <nekomarisakirisame@gmail.com>
2023-12-11 20:23:37 -0500

    Add basic identify packet

---
+++ Kumi.Game/Online/Server/Packets/IdentifyPacket.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Kumi.Game.Online.Server.Packets;
+
+public class IdentifyPacket : Packet<IdentifyPacket.IdentifyData>
+{
+    public class IdentifyData
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+}
