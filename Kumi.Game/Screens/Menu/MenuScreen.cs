33da425b7485bc2457b33170955f11447e07b0bc
Ryōshū <kyoko.laica@gmail.com>
2023-12-14 18:07:43 -0500

    Menu buttons

--- Kumi.Game/Screens/Menu/MenuScreen.cs
+++ Kumi.Game/Screens/Menu/MenuScreen.cs
@@ -2,8 +2,9 @@ using Kumi.Game.Graphics;
 using Kumi.Game.Graphics.Backgrounds;
 using Kumi.Game.Overlays;
 using Kumi.Game.Screens.Backgrounds;
+using Kumi.Game.Screens.Edit;
+using Kumi.Game.Screens.Select;
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -17,21 +18,25 @@ namespace Kumi.Game.Screens.Menu;
 
 public partial class MenuScreen : KumiScreen
 {
-    public override bool HideOverlaysOnEnter => true;
+    public override bool HideOverlaysOnEnter => buttons == null || buttons.State == ButtonsState.Hidden;
     protected override OverlayActivation InitialOverlayActivation => Overlays.OverlayActivation.UserTriggered;
 
     public override BackgroundScreen CreateBackground() => new MenuBackground();
+
+    private SelectScreen? selectScreen;
 
     private Container logoContainer = null!;
     private Sprite logo = null!;
     private Sprite logoShadow = null!;
     private SpriteText ctaText = null!;
 
-    private Bindable<MenuState> state = new Bindable<MenuState>();
+    private MenuButtons? buttons;
 
     [Resolved]
     private MusicController musicController { get; set; } = null!;
 
+    [Resolved]
+    private TaskbarOverlay taskbar { get; set; } = null!;
 
     [BackgroundDependencyLoader]
     private void load(LargeTextureStore store)
@@ -41,8 +46,8 @@ public partial class MenuScreen : KumiScreen
             logoContainer = new Container
             {
                 RelativeSizeAxes = Axes.Both,
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
                 Name = "Logo container",
                 Children = new Drawable[]
                 {
@@ -66,6 +71,20 @@ public partial class MenuScreen : KumiScreen
                     }
                 }
             },
+            new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                Height = 0.75f,
+                Anchor = Anchor.BottomCentre,
+                Origin = Anchor.BottomCentre,
+                Child = buttons = new MenuButtons
+                {
+                    Height = 0.75f,
+                    OnPlay = () => this.Push(consumeSelect()),
+                    OnEdit = () => this.Push(new Editor()),
+                    OnExit = this.Exit
+                }
+            },
             ctaText = new SpriteText
             {
                 Shadow = true,
@@ -84,6 +103,22 @@ public partial class MenuScreen : KumiScreen
                 }
             },
         });
+
+        buttons.StateChanged += onStateChanged;
+        preloadSelect();
+    }
+
+    private void preloadSelect()
+    {
+        if (selectScreen == null)
+            LoadComponentAsync(selectScreen = new SelectScreen());
+    }
+
+    private Screen consumeSelect()
+    {
+        var s = selectScreen;
+        selectScreen = null;
+        return s!;
     }
 
     public override void OnEntering(ScreenTransitionEvent e)
@@ -99,6 +134,40 @@ public partial class MenuScreen : KumiScreen
         musicController.EnsurePlayingSong();
     }
 
+    public override void OnResuming(ScreenTransitionEvent e)
+    {
+        base.OnResuming(e);
+
+        musicController.EnsurePlayingSong();
+        preloadSelect();
+    }
+
+    private const float duration = 500f;
+
+    private void onStateChanged(ButtonsState state)
+    {
+        switch (state)
+        {
+            case ButtonsState.Hidden:
+                ctaText.FadeIn(duration, Easing.OutQuint);
+                ctaText.MoveToY(-32, duration, Easing.OutQuint);
+                logoContainer.ResizeHeightTo(1f, duration, Easing.OutQuint);
+
+                OverlayActivation.Value = Overlays.OverlayActivation.UserTriggered;
+                taskbar.Hide();
+                break;
+
+            case ButtonsState.Visible:
+                ctaText.FadeOut(duration, Easing.OutQuint);
+                ctaText.MoveToY(0, duration, Easing.OutQuint);
+                logoContainer.ResizeHeightTo(0.3f, duration, Easing.OutQuint);
+
+                OverlayActivation.Value = Overlays.OverlayActivation.Any;
+                taskbar.Show();
+                break;
+        }
+    }
+
     private partial class MenuBackground : BackgroundScreen
     {
         [BackgroundDependencyLoader]
@@ -109,11 +178,5 @@ public partial class MenuScreen : KumiScreen
 
             background.SetBackground(store.Get("Backgrounds/bg1"));
         }
-    }
-
-    private enum MenuState
-    {
-        Logo,
-        Menu
     }
 }
