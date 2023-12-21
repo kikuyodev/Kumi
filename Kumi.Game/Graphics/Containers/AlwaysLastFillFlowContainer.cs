35e8fbb73b4df806963aba18b56d58dbaf29974b
Ryōshū <kyoko.laica@gmail.com>
2023-12-21 15:57:53 -0500

    Add a way to add and remove timing points

---
+++ Kumi.Game/Graphics/Containers/AlwaysLastFillFlowContainer.cs
@@ -0,0 +1,57 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace Kumi.Game.Graphics.Containers;
+
+public partial class AlwaysLastFillFlowContainer : FillFlowContainer
+{
+    private Func<Drawable>? createComponent;
+
+    public Func<Drawable>? CreateComponent
+    {
+        get => createComponent;
+        set
+        {
+            component?.Expire();
+            component = value?.Invoke();
+
+            createComponent = value;
+        }
+    }
+
+    private Drawable? component;
+
+    public AlwaysLastFillFlowContainer(Func<Drawable>? createComponent = null)
+    {
+        CreateComponent = createComponent;
+    }
+
+    private bool isBatch;
+
+    public new void AddRange(IEnumerable<Drawable> drawables)
+    {
+        isBatch = true;
+        
+        base.AddRange(drawables);
+        
+        component?.Expire();
+        component = CreateComponent?.Invoke();
+
+        if (component != null)
+            base.Add(component);
+    }
+
+    public override void Add(Drawable drawable)
+    {
+        if (!isBatch)
+        {
+            component?.Expire();
+            component = CreateComponent?.Invoke();
+        }
+
+        base.Add(drawable);
+
+        if (component != null && !isBatch)
+            base.Add(component);
+    }
+}
