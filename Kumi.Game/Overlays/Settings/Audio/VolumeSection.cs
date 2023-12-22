1efe1c428d53ba42bf9959504b40ad7aab2aec49
Ryōshū <kyoko.laica@gmail.com>
2023-12-22 16:11:20 -0500

    Audio settings

---
+++ Kumi.Game/Overlays/Settings/Audio/VolumeSection.cs
@@ -0,0 +1,35 @@
+﻿using Kumi.Game.Overlays.Settings.Components;
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Graphics;
+using osu.Framework.Localisation;
+
+namespace Kumi.Game.Overlays.Settings.Audio;
+
+public partial class VolumeSection : SettingSection
+{
+    protected override LocalisableString Header => "Volume";
+
+    [BackgroundDependencyLoader]
+    private void load(AudioManager audio)
+    {
+        Children = new Drawable[]
+        {
+            new SliderBarSettingItem<double>
+            {
+                Label = "Master Volume",
+                Current = audio.Volume
+            },
+            new SliderBarSettingItem<double>
+            {
+                Label = "Effect Volume",
+                Current = audio.VolumeSample
+            },
+            new SliderBarSettingItem<double>
+            {
+                Label = "Music Volume",
+                Current = audio.VolumeTrack
+            }
+        };
+    }
+}
---
+++ Kumi.Game/Overlays/Settings/AudioSettings.cs
@@ -0,0 +1,17 @@
+﻿using Kumi.Game.Overlays.Settings.Audio;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+
+namespace Kumi.Game.Overlays.Settings;
+
+public partial class AudioSettings : SettingScreen
+{
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        Children = new Drawable[]
+        {
+            new VolumeSection()
+        };
+    }
+}
