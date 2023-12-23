bbb9a81b41476b0ab870df2d3a71c82ada082820
Irisu <nekomarisakirisame@gmail.com>
2023-12-23 15:28:15 -0500

    Add initial squirrel stuff, and seperate debug builds from release builds

--- Kumi.Desktop/Program.cs
+++ Kumi.Desktop/Program.cs
@@ -1,15 +1,44 @@
 ﻿using Kumi.Game;
 using osu.Framework;
+using osu.Framework.Development;
+using Squirrel;
 
 namespace Kumi.Desktop;
 
 public static class Program
 {
+#if DEBUG
+    public const string APP_NAME = "Kumi Development";
+#else
+    public const string APP_NAME = "Kumi";
+#endif
+
     public static void Main(string[] args)
     {
-        using var host = Host.GetSuitableDesktopHost("Kumi");
+        using var host = Host.GetSuitableDesktopHost(APP_NAME);
         using var game = new KumiGame();
 
+        handleSquirrelEvents();
         host.Run(game);
+    }
+
+    private static void handleSquirrelEvents()
+    {
+        SquirrelAwareApp.HandleEvents(
+            onInitialInstall: (_, tools) =>
+            {
+                tools.CreateShortcutForThisExe();
+                tools.CreateUninstallerRegistryEntry();
+            },
+            onAppUninstall: (_, tools) =>
+            {
+                tools.RemoveShortcutForThisExe();
+                tools.RemoveUninstallerRegistryEntry();
+            },
+            onAppUpdate: (_, tools) =>
+            {
+                tools.CreateUninstallerRegistryEntry();
+            }
+        );
     }
 }
