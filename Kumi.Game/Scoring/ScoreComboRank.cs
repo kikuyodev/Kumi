3cda72c3d956be83bedb4a2907276e88a157896b
Ryōshū <kyoko.laica@gmail.com>
2023-12-25 19:03:24 -0500

    Add descriptions to score rank enums

--- Kumi.Game/Scoring/ScoreComboRank.cs
+++ Kumi.Game/Scoring/ScoreComboRank.cs
@@ -1,8 +1,15 @@
-﻿namespace Kumi.Game.Scoring;
+﻿using System.ComponentModel;
+
+namespace Kumi.Game.Scoring;
 
 public enum ScoreComboRank
 {
+    [Description("")]
     Clear,
+    
+    [Description("FC")]
     FullCombo,
+    
+    [Description("PC")]
     PerfectCombo
 }
--- Kumi.Game/Scoring/ScoreRank.cs
+++ Kumi.Game/Scoring/ScoreRank.cs
@@ -1,11 +1,24 @@
-﻿namespace Kumi.Game.Scoring;
+﻿using System.ComponentModel;
+
+namespace Kumi.Game.Scoring;
 
 public enum ScoreRank
 {
+    [Description("Iki")]
     Iki,
+
+    [Description("Iki+")]
     IkiBronze,
+
+    [Description("Iki++")]
     IkiSilver,
+
+    [Description("Miyabi")]
     MiyabiGold,
+
+    [Description("Miyabi+")]
     MiyabiPlatinum,
+
+    [Description("Kiwami")]
     Kiwami
 }
